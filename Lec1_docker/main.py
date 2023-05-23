dc1ad9c5768c58b692e680f020499aa88ca406fa
smlatic <edin.smlatic@gmail.com>
2023-05-23 11:55:01 +0200

    docker code along

--- Lec1_docker/main.py
+++ Lec1_docker/main.py
@@ -1,8 +1,18 @@
-import sys
+import plotly_express
+import numpy as np
+from dash import Dash, dcc
+from dash.html import H1, Div, P, H2
 
-print('Python version')
-print(sys.version)
-print('Version info.')
-print(sys.version_info)
+app = Dash(__name__)
 
-print("Print från Main py.")
+app.layout = Div([
+    H1("Dice simulator"),
+    P("Choose number of dice and number of rolls and enjoy the results"),
+    H2("Number of rolls ")
+])
+
+
+if __name__ == "__main__":
+    print('Hello from the docker side')
+
+    app.run_server(debug=True)
